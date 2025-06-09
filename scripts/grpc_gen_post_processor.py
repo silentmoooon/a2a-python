d537285b233d301c7bf0f8ba10eeb9367131b39a
Rajesh Velicheti <rajesh.velicheti@gmail.com>
2025-06-10 00:36:46 +0530

    chore: Post processor script for grpc generation. (#170)
    # Description
    
    Thank you for opening a Pull Request!
    Before submitting your PR, there are a few things you can do to make
    sure it goes smoothly:
    
    - [ ] Follow the [`CONTRIBUTING`
    Guide](https://github.com/google-a2a/a2a-python/blob/main/CONTRIBUTING.md).
    - [ ] Make your Pull Request title in the
    <https://www.conventionalcommits.org/> specification.
    - Important Prefixes for
    [release-please](https://github.com/googleapis/release-please):
    - `fix:` which represents bug fixes, and correlates to a
    [SemVer](https://semver.org/) patch.
    - `feat:` represents a new feature, and correlates to a SemVer minor.
    - `feat!:`, or `fix!:`, `refactor!:`, etc., which represent a breaking
    change (indicated by the `!`) and will result in a SemVer major.
    - [ ] Ensure the tests and linter pass (Run `nox -s format` from the
    repository root to format)
    - [ ] Appropriate docs were updated (if necessary)
    
    Fixes #<issue_number_goes_here> 🦕

---
+++ scripts/grpc_gen_post_processor.py
@@ -0,0 +1,56 @@
+"""
+Fix absolute imports in *_pb2_grpc.py files.
+Example:
+import a2a_pb2 as a2a__pb2
+from . import a2a_pb2 as a2a__pb2
+"""
+
+import re
+import sys
+
+from pathlib import Path
+
+
+def process_generated_code(src_folder: str = 'src/a2a/grpc'):
+    """Post processor for the generated code."""
+    dir_path = Path(src_folder)
+    print(dir_path)
+    if not dir_path.is_dir():
+      print('Source folder not found')
+      sys.exit(1)
+
+    grpc_pattern = '**/*_pb2_grpc.py'
+    files = dir_path.glob(grpc_pattern)
+
+    for file in files:
+      print(f'Processing {file}')
+      try:
+        with file.open('r', encoding='utf-8') as f:
+          src_content = f.read()
+
+          # Change import a2a_pb2 as a2a__pb2
+          import_pattern = r'^import (\w+_pb2) as (\w+__pb2)$'
+          # to from . import a2a_pb2 as a2a__pb2
+          replacement_pattern = r'from . import \1 as \2'
+
+          fixed_src_content = re.sub(
+            import_pattern,
+            replacement_pattern,
+            src_content,
+            flags=re.MULTILINE,
+          )
+
+          if fixed_src_content != src_content:
+            with file.open('w', encoding='utf-8') as f:
+              f.write(fixed_src_content)
+              print('Imports fixed')
+          else:
+              print('No changes needed')
+
+      except Exception as e:
+        print(f'Error processing file {file}: {e}')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    process_generated_code()
